36efdc061f8d069e964787bcab37f3d453387ded
Julia Ruprecht <89738714+Noonchi1004@users.noreply.github.com>
2022-07-06 13:19:02 +0200

    Update RunMojo.java

--- devtools/maven/src/main/java/io/quarkus/maven/RunMojo.java
+++ devtools/maven/src/main/java/io/quarkus/maven/RunMojo.java
@@ -2,6 +2,10 @@ package io.quarkus.maven;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.*;
+import java.util.jar.JarException;
+import java.util.jar.JarFile;
+
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.shared.utils.cli.CommandLineUtils;
 import org.apache.maven.project.MavenProject;
@@ -10,25 +14,32 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+
 @Mojo(name = "run")
 public class RunMojo extends AbstractMojo {
     private Process process;
     protected MavenProject project;
     private File outputDirectory;
+    private Path path;
+
+    public boolean isPathValid() {
+        return Files.exists(Paths.get(project.getBuild().getDirectory()));
+}
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         Process app = null;
+        if (!isPathValid()) {
+           throw new MojoExecutionException("Failed to find valid file Path"); //add more precise comment + file
+        }
         try {
             app = new ProcessBuilder()
-                    .command("java", "-jar", "target/quarkus-app/quarkus-run.jar") //getAbsolutePath()?
+                    .command("JavaBinFinder.findBin()", "-jar", "target/quarkus-app/quarkus-run.jar") //getAbsolutePath()?
                     .inheritIO()
                     .redirectErrorStream(true)
                     .start();
-                    /*.directory(workingDir == null ? project.getBasedir() : workingDir);
-            if (!environmentVariables.isEmpty()) {
-                processBuilder.environment().putAll(environmentVariables);
-            } */
             app.waitFor();
+
         } catch (IOException e) {
             throw new MojoExecutionException("Failed to launch the application ｡ﾟ(ﾟஇ‸இﾟ)ﾟ｡", e);
         } catch (InterruptedException e) {
@@ -41,13 +52,13 @@ public class RunMojo extends AbstractMojo {
                 try {
                     process.waitFor();
                 } catch (InterruptedException e) {
-                    getLog().warn("Unable to properly wait for dev-mode end", e);
+                    getLog().warn("Unable to properly wait for run-mode end", e);
                 }
             }
         }, "Development Mode Shutdown Hook"));
-        void stop() throws InterruptedException {
+        /*void stop() throws InterruptedException { //check errors
             process.destroy();
             process.waitFor();
-        }
+        }*/
     }
 }
